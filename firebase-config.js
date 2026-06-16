fc68bfc03c50847b472eceea6cd011b0e939998d
en6dea <en6dea@gmail.com>
2026-06-16 17:40:58 +0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-// Second Brain OS Cloud Sync config
-// 1) Создай Firebase project
-// 2) Project settings → General → Your apps → Web app → Firebase SDK config
-// 3) Замени значения ниже на свои
 window.SECOND_BRAIN_FIREBASE_CONFIG = {
   apiKey: "PASTE_API_KEY_HERE",
   authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
