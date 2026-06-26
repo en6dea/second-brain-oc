024f8071744b3a3be4ac3b388c16b1169e05344c
en6dea <en6dea@gmail.com>
2026-06-26 11:08:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'second-brain-os-v22-notion-dashboard-fix-20260626';
+const CACHE_NAME = 'second-brain-os-v23-reference-match-20260626';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
