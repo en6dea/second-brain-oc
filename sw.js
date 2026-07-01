0b72b9a5f15cec0f5b4430d5aa9f7972ab970afa
en6dea <en6dea@gmail.com>
2026-07-01 18:03:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='second-brain-os-v32-buttons-only-hard-fix-20260701';
-const ASSETS=['./','./index.html','./app-v32-root-cause-fix.js','./app.js','./manifest.webmanifest','./offline.html'];
+const CACHE_NAME='second-brain-os-v33-polina-root-fix-20260701';
+const ASSETS=['./','./index.html','./app-v33-polina-root-fix.js','./app.js','./manifest.webmanifest','./offline.html'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS).catch(()=>{}))) });
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const clone=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,clone)).catch(()=>{});return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./offline.html'))))});
